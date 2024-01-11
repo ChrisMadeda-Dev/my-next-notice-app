b023938112d7435f5d19ee12913dfe0b7dd74d20
ChrisMadeda-Dev <chrismadeda.dev@gmail.com>
2024-01-11 23:00:15 +0300

    working on the ui

---
+++ app/components/Notice/NoticeBoard.jsx
@@ -0,0 +1,9 @@
+import NoticeCont from "./NoticeCont";
+
+export default function NoticeBoard() {
+  return (
+    <div className="h-5/6 w-4/5 bg-blue-300 flex flex-col justify-start items-center gap-8 p-10">
+      <NoticeCont />
+    </div>
+  );
+}
---
+++ app/components/Notice/NoticeCont.jsx
@@ -0,0 +1,3 @@
+export default function NoticeCont() {
+  return <div className="w-full h-1/2 bg-white rounded-lg"></div>;
+}
